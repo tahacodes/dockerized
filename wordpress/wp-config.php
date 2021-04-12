407e723959cbde93ab60db264228a971b38c3322
Taha Farahani <iotahacodes@gmail.com>
2021-04-12 14:38:42 +0430

    change DB_HOST to static ip

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -2,7 +2,7 @@
 define( 'DB_NAME', 'wordpress' );
 define( 'DB_USER', 'wordpress' );
 define( 'DB_PASSWORD', 'wordpress' );
-define( 'DB_HOST', 'db:3306' );
+define( 'DB_HOST', '172.100.1.3:3306' );
 define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
 
